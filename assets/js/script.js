c1c75cb036163c04ba53a6ed70e209836cac4df0
Kevin Lourenço <kevinsplourenco@gmail.com>
2024-02-04 19:59:01 -0300

    final version

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,19 @@ function changeTheme() {
   
 
 toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
